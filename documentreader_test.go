320d6e5d1d8df936bee37ef35cb31039db3b8eed
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-10-12 19:15:49 +0300

    test ReadLimited

--- documentreader_test.go
+++ documentreader_test.go
@@ -8,6 +8,52 @@ import (
 	"testing"
 )
 
+func TestReadLimited(t *testing.T) {
+	var tests = []struct {
+		document    string
+		limit       int64
+		contentPath string
+		checker     func(xml.StartElement) bool
+		golden      string
+	}{
+		{"odt/document1.odt", 700, OdtContentPath, IsOdtText, "odt/title1.golden"},
+		{"odt/document3.odt", 700, OdtContentPath, IsOdtText, "odt/title3.golden"},
+		{"odt/document4.odt", 700, OdtContentPath, IsOdtText, "odt/title4.golden"},
+		{"odt/document4.odt", 9999, OdtContentPath, IsOdtText, "odt/title4.golden"},
+		{"docx/document1.docx", 700, DocxContentPath, IsDocxText, "docx/title1.golden"},
+		{"docx/document2.docx", 700, DocxContentPath, IsDocxText, "docx/title2.golden"},
+		{"docx/document3.docx", 700, DocxContentPath, IsDocxText, "docx/title3.golden"},
+	}
+
+	for _, tt := range tests {
+		file, err := os.Open("testdata/" + tt.document)
+		if err != nil {
+			t.Fatalf("Failed to read %s: %v", tt.document, err)
+		}
+		defer file.Close()
+
+		fi, err := file.Stat()
+		if err != nil {
+			t.Fatalf("Failed to get %s info: %v", tt.document, err)
+		}
+		size := fi.Size()
+
+		want, err := os.ReadFile("testdata/" + tt.golden)
+		if err != nil {
+			t.Fatalf("Failed to read %s: %v", tt.document, err)
+		}
+
+		got, err := ReadLimited(file, size, tt.limit, tt.contentPath, tt.checker)
+		if err != nil && err != io.ErrUnexpectedEOF {
+			t.Fatalf("Failed to parse text in %s: %v", tt.document, err)
+		}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("\n%s:\n\t%s\n\n%s:\n\t%s\n", tt.document, got, tt.golden, want)
+		}
+	}
+}
+
 func TestReadContentLimited(t *testing.T) {
 	var tests = []struct {
 		document string
@@ -15,13 +61,13 @@ func TestReadContentLimited(t *testing.T) {
 		checker  func(xml.StartElement) bool
 		golden   string
 	}{
-		{"docx/content1.xml", 700, IsDocxText, "docx/title1.golden"},
-		{"docx/content2.xml", 700, IsDocxText, "docx/title2.golden"},
-		{"docx/content3.xml", 700, IsDocxText, "docx/title3.golden"},
 		{"odt/content1.xml", 700, IsOdtText, "odt/title1.golden"},
 		{"odt/content3.xml", 700, IsOdtText, "odt/title3.golden"},
 		{"odt/content4.xml", 700, IsOdtText, "odt/title4.golden"},
 		{"odt/content4.xml", 9999, IsOdtText, "odt/title4.golden"},
+		{"docx/content1.xml", 700, IsDocxText, "docx/title1.golden"},
+		{"docx/content2.xml", 700, IsDocxText, "docx/title2.golden"},
+		{"docx/content3.xml", 700, IsDocxText, "docx/title3.golden"},
 	}
 
 	for _, tt := range tests {
@@ -33,11 +79,11 @@ func TestReadContentLimited(t *testing.T) {
 
 		want, err := os.ReadFile("testdata/" + tt.golden)
 		if err != nil {
-			t.Fatalf("Failed to open %s: %v", tt.document, err)
+			t.Fatalf("Failed to read %s: %v", tt.document, err)
 		}
 
 		got, err := readContentLimited(file, tt.limit, tt.checker)
-		if err != nil  && err != io.ErrUnexpectedEOF{
+		if err != nil && err != io.ErrUnexpectedEOF {
 			t.Fatalf("Failed to parse text in %s: %v", tt.document, err)
 		}
 
