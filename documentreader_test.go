c40cf78d9961f0a861494bcd2fba3903cebaef15
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-10-16 15:41:03 +0300

    test read document for UnexpectedEOF

--- documentreader_test.go
+++ documentreader_test.go
@@ -9,22 +9,19 @@ import (
 	"testing"
 )
 
-func TestReadLimited(t *testing.T) {
+func TestReadLimited_Success(t *testing.T) {
 	var tests = []struct {
 		document    string
 		limit       int64
 		contentPath string
 		checker     func(xml.StartElement) bool
 		golden      string
-		expectedErr error
 	}{
-		{"odt/document1.odt", 700, OdtContentPath, IsOdtText, "odt/title1.golden", nil},
-		{"odt/document3.odt", 700, OdtContentPath, IsOdtText, "odt/title3.golden", nil},
-		{"docx/document1.docx", 700, DocxContentPath, IsDocxText, "docx/title1.golden", nil},
-		{"docx/document2.docx", 700, DocxContentPath, IsDocxText, "docx/title2.golden", nil},
-		{"docx/document3.docx", 700, DocxContentPath, IsDocxText, "docx/title3.golden", nil},
-
-		{"odt/document4.odt", 9999, OdtContentPath, IsOdtText, "odt/title4.golden", io.ErrUnexpectedEOF},
+		{"odt/document1.odt", 700, OdtContentPath, IsOdtText, "odt/title1.golden"},
+		{"odt/document3.odt", 700, OdtContentPath, IsOdtText, "odt/title3.golden"},
+		{"docx/document1.docx", 700, DocxContentPath, IsDocxText, "docx/title1.golden"},
+		{"docx/document2.docx", 700, DocxContentPath, IsDocxText, "docx/title2.golden"},
+		{"docx/document3.docx", 700, DocxContentPath, IsDocxText, "docx/title3.golden"},
 	}
 
 	for _, tt := range tests {
@@ -41,8 +38,8 @@ func TestReadLimited(t *testing.T) {
 		size := fi.Size()
 
 		got, err := ReadLimited(file, size, tt.limit, tt.contentPath, tt.checker)
-		if err != tt.expectedErr {
-			t.Fatalf("Reading %s got error '%v', want '%v'", tt.document, err, tt.expectedErr)
+		if err != nil {
+			t.Fatalf("Unexpected error reading %s: %v", tt.document, err)
 		}
 
 		want, err := os.ReadFile("testdata/" + tt.golden)
@@ -53,6 +50,37 @@ func TestReadLimited(t *testing.T) {
 		if !slices.Equal(got, want) {
 			t.Errorf("\n%s:\n\t%s\n\n%s:\n\t%s\n", tt.document, got, tt.golden, want)
 		}
+	}
+}
+
+func TestReadLimited_Error_UnexpectedEOF(t *testing.T) {
+	document := "odt/document4.odt"
+	golden := "odt/title4.golden"
+
+	file, err := os.Open("testdata/" + document)
+	if err != nil {
+		t.Fatalf("Failed to read %s: %v", document, err)
+	}
+	defer file.Close()
+
+	fi, err := file.Stat()
+	if err != nil {
+		t.Fatalf("Failed to get %s info: %v", document, err)
+	}
+	size := fi.Size()
+
+	got, err := ReadLimited(file, size, 99999, OdtContentPath, IsOdtText)
+	if err != io.ErrUnexpectedEOF {
+		t.Fatalf("Unexpected error reading %s: %v", document, err)
+	}
+
+	want, err := os.ReadFile("testdata/" + golden)
+	if err != nil {
+		t.Fatalf("Failed to read %s: %v", document, err)
+	}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("\n%s:\n\t%s\n\n%s:\n\t%s\n", document, got, golden, want)
 	}
 }
 
