609efbcf9004fc83677d6356cfc86dc31c68f6ac
trust-me-im-an-engineer <isavelij4@gmail.com>
2025-10-23 15:57:55 +0300

    lowercase errors

--- documentreader_test.go
+++ documentreader_test.go
@@ -29,24 +29,24 @@ func TestReadLimited_Success(t *testing.T) {
 		t.Run(tt.document, func(t *testing.T) {
 			file, err := os.Open("testdata/" + tt.document)
 			if err != nil {
-				t.Fatalf("Failed to read %s: %v", tt.document, err)
+				t.Fatalf("failed to read %s: %v", tt.document, err)
 			}
 			defer file.Close()
 
 			fi, err := file.Stat()
 			if err != nil {
-				t.Fatalf("Failed to get %s info: %v", tt.document, err)
+				t.Fatalf("failed to get %s info: %v", tt.document, err)
 			}
 			size := fi.Size()
 
 			got, err := readLimited(file, size, tt.limit, tt.contentPath, tt.checker)
 			if err != nil {
-				t.Fatalf("Unexpected error reading %s: %v", tt.document, err)
+				t.Fatalf("unexpected error reading %s: %v", tt.document, err)
 			}
 
 			want, err := os.ReadFile("testdata/" + tt.golden)
 			if err != nil {
-				t.Fatalf("Failed to read %s: %v", tt.document, err)
+				t.Fatalf("failed to read %s: %v", tt.document, err)
 			}
 
 			if diff := cmp.Diff(want, got); diff != "" {
@@ -62,24 +62,24 @@ func TestReadLimited_Error_UnexpectedEOF(t *testing.T) {
 
 	file, err := os.Open("testdata/" + document)
 	if err != nil {
-		t.Fatalf("Failed to read %s: %v", document, err)
+		t.Fatalf("failed to read %s: %v", document, err)
 	}
 	defer file.Close()
 
 	fi, err := file.Stat()
 	if err != nil {
-		t.Fatalf("Failed to get %s info: %v", document, err)
+		t.Fatalf("failed to get %s info: %v", document, err)
 	}
 	size := fi.Size()
 
 	got, err := readLimited(file, size, 99999, contentPathODT, isODT)
 	if err != io.ErrUnexpectedEOF {
-		t.Fatalf("Unexpected error reading %s: %v", document, err)
+		t.Fatalf("unexpected error reading %s: %v", document, err)
 	}
 
 	want, err := os.ReadFile("testdata/" + golden)
 	if err != nil {
-		t.Fatalf("Failed to read %s: %v", document, err)
+		t.Fatalf("failed to read %s: %v", document, err)
 	}
 
 	if diff := cmp.Diff(want, got); diff != "" {
@@ -105,18 +105,18 @@ func TestReadContentLimited_Success(t *testing.T) {
 		t.Run(tt.document, func(t *testing.T) {
 			file, err := os.Open("testdata/" + tt.document)
 			if err != nil {
-				t.Fatalf("Failed to open %s: %v", tt.document, err)
+				t.Fatalf("failed to open %s: %v", tt.document, err)
 			}
 			defer file.Close()
 
 			got, err := readContentLimited(file, tt.limit, tt.checker)
 			if err != nil {
-				t.Fatalf("Failed to read content from %v: %v", tt.document, err)
+				t.Fatalf("failed to read content from %v: %v", tt.document, err)
 			}
 
 			want, err := os.ReadFile("testdata/" + tt.golden)
 			if err != nil {
-				t.Fatalf("Failed to read %s: %v", tt.document, err)
+				t.Fatalf("failed to read %s: %v", tt.document, err)
 			}
 
 			if diff := cmp.Diff(want, got); diff != "" {
@@ -132,19 +132,18 @@ func TestReadContentLimited_Error_UnexpectedEOF(t *testing.T) {
 
 	file, err := os.Open("testdata/" + content)
 	if err != nil {
-		t.Fatalf("Failed to open %s: %v", content, err)
+		t.Fatalf("failed to open %s: %v", content, err)
 	}
 	defer file.Close()
 
 	got, err := readContentLimited(file, 99999, isODT)
-
 	if err != io.ErrUnexpectedEOF {
-		t.Fatalf("Unexpected error reading %s: %v", content, err)
+		t.Fatalf("unexpected error reading %s: %v", content, err)
 	}
 
 	want, err := os.ReadFile("testdata/" + golden)
 	if err != nil {
-		t.Fatalf("Failed to read %s: %v", golden, err)
+		t.Fatalf("failed to read %s: %v", golden, err)
 	}
 
 	if diff := cmp.Diff(want, got); diff != "" {
@@ -156,7 +155,7 @@ func TestReadContentLimited_Error_Syntax(t *testing.T) {
 	content := "odt/invalid.xml"
 	file, err := os.Open("testdata/" + content)
 	if err != nil {
-		t.Fatalf("Failed to open %s: %v", content, err)
+		t.Fatalf("failed to open %s: %v", content, err)
 	}
 	defer file.Close()
 
